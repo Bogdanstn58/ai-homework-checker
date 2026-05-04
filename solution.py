b11a50ddb7dd9fab1c55c5d010ffeaf601677da2
Bogdanstn58 <bogdan.valyaev@yandex.ru>
2026-05-04 17:15:29 +0300

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,6 @@
+def count_substring_occurrences(main_string, sub_string):
+    """
+    Возвращает количество вхождений sub_string в main_string
+    (без учёта регистра). Сейчас заглушка для прохождения импорта.
+    """
+    return 0
