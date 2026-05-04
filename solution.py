79634b1a371cbebb6eb0a16b53c80478a686963a
Bogdanstn58 <bogdan.valyaev@yandex.ru>
2026-05-04 17:20:08 +0300

    Delete solution.py

--- solution.py
+++
@@ -1,6 +0,0 @@
-def count_substring_occurrences(main_string, sub_string):
-    """
-    Возвращает количество вхождений sub_string в main_string
-    (без учёта регистра). Сейчас заглушка для прохождения импорта.
-    """
-    return 0
